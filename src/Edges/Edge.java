316d3c7fd7f3db52584de24f3fcbc009e4874031
Henrique Pizzol Grando <hp2409@columbia.edu>
2017-04-20 14:01:09 -0300

    Scan line algorithm implemented

--- src/Edges/Edge.java
+++ src/Edges/Edge.java
@@ -1,20 +1,20 @@
 package edges;
 import java.lang.Math;
 
-//Note: this class has a natural ordering that is inconsistent with equals.
+// Note: this class has a natural ordering that is inconsistent with equals.
 public class Edge implements Comparable<Edge>{
 	int ymax;
 	int xmin;
 	int num;	// numerator
 	int den;	// and denominator of the (edge's angular coeficient) ^ -1 --> 1/m
-	int increment; // increment different than zero for edges with angular coeficient between (0,1)
+	int increment;
 
 	public Edge(int ymax, int xmin, int num, int den){
 		this.ymax = ymax;
 		this.xmin = xmin;
 		this.num = num;
 		this.den = den;
-		this.increment = num/den;
+		this.inc = den;
 	}
 
 	public int getYmax(){
@@ -35,11 +35,28 @@ public class Edge implements Comparable<Edge>{
 
 	// Updates Edge's xmin considering an increment of 1 in y cordinate
 	public void scan(){
-		// num += num;
-		//
-		// if (Math.abs(num) > den){
-		// 	num = den
-		// }
+
+		// Left and top edge scan
+		if (num > 0 && den > 0){
+			inc += num;
+
+			// Loop run more than once for top edge
+			while (inc > den){
+				inc -= den;
+				xmin++;
+			}
+		}
+
+		// Right and bottom edge scan
+		else {
+			inc -= num;
+
+			// Loop run more than once for bottom edge
+			while (Math.abs(inc) > Math.abs(den)){
+				inc -= den;
+				xmin--;
+			}
+		}
 	}
 
 	// Compare edges by its xmin parameters
